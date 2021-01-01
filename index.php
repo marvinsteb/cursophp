3de7171f743e9c0849356404e13f0315e1a2b5af
Marvin Menchu <mmenchu@outlook.com>
2020-12-31 23:09:32 -0600

    pruebas con array_key_exists in_array y array_values

--- index.php
+++ index.php
@@ -11,3 +11,25 @@ array_walk($cursos,function($curso,$llave){
 echo strtoupper($curso) . ": $llave <br>";
 });
 
+if(array_key_exists('frontend',$cursos)){
+  echo "la llave frontend existe en el array";
+} else{
+  echo "la lave frontend no existe en el array";
+};
+
+
+if(in_array('javascript',$cursos)){
+  echo "el valor javascript existe en el array";
+} else {
+  echo "el valor javascript no existe en el array";
+}
+echo "<h2>llaves</h2>";
+echo "<pre>";
+var_dump(array_keys($cursos));
+echo "</pre>";
+
+echo "<h2>valores</h2>";
+echo "<pre>";
+var_dump(array_values($cursos));
+echo "</pre>";
+
